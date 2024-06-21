e79bac16f8812f6484919b048011b174b94bd01b
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-21 18:25:32 +0200

    [css] home components

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,18 @@
 import "./home.scss";
 const Home = () => {
-  return <div className="home">HOME</div>;
+  return (
+    <div className="home">
+      <div className="box box1">BOX1</div>
+      <div className="box box2">BOX2</div>
+      <div className="box box3">BOX3</div>
+      <div className="box box4">BOX4</div>
+      <div className="box box5">BOX5</div>
+      <div className="box box6">BOX6</div>
+      <div className="box box7">BOX7</div>
+      <div className="box box8">BOX8</div>
+      <div className="box box9">BOX9</div>
+    </div>
+  );
 };
 
 export default Home;
