ec4c287faf57c2a87bbbb354d0d867fb5cfb640d
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-21 17:24:19 +0200

    [add] navbar_menu,foter, header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import Home from "./pages/home/Home";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import Users from "./pages/users/Users";
 import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Menu from "./components/menu/Menu";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
-    },
-    {
-      path: "users",
-      element: <Users />,
-    },
-    {
-      path: "products",
-      element: <Products />,
+      element: <Layout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/login", element: <Login /> },
+        { path: "/users", element: <Users /> },
+        { path: "/products", element: <Products /> },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
