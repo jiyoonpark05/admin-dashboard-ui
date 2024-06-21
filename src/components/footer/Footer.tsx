1d23268b6e1df92d53d3023a0263ff927706c386
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-21 17:27:25 +0200

    [css] footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import "./footer.scss";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>admin</span>
+      <span>@ ADMIN DASHBOARD</span>
+    </div>
+  );
 };
 
 export default Footer;
