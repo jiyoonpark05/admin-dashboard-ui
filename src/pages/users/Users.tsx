3c3d4e2ff8c9b3163fd154a396334c5dece02a3b
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-24 18:58:48 +0200

    [add] add new user modal

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,72 +2,66 @@ import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
 import { userRows } from "../../data";
+import { useState } from "react";
+import Add from "../../components/add/Add";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 const Users = () => {
-  const columns: GridColDef<(typeof rows)[number]>[] = [
-    { field: "id", headerName: "ID", width: 90 },
-    {
-      field: "avatar",
-      headerName: "Avatar",
-      width: 100,
-      renderCell: (params) => {
-        return <img src={params.row.img || "/noavatar.png"} alt="" />;
-      },
-    },
-    {
-      field: "actions",
-      headerName: "Actions",
-      width: 100,
-      renderCell: (params) => {
-        return (
-          <div className="action">
-            <div className="view">View</div>
-            <div className="delete">Delete</div>
-          </div>
-        );
-      },
-    },
-    {
-      field: "status",
-      headerName: "Status",
-      width: 100,
-      type: "boolean",
-    },
-    {
-      field: "firstName",
-      headerName: "First name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "lastName",
-      headerName: "Last name",
-      width: 150,
-      editable: true,
-    },
-    {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      width: 110,
-      editable: true,
-    },
-    {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      width: 160,
-      valueGetter: (value, row) =>
-        `${row.firstName || ""} ${row.lastName || ""}`,
-    },
-  ];
+  const [open, setOpen] = useState(false);
   return (
     <div className="users">
       <div className="info">
         <h1>Users</h1>
-        <button>Add new User</button>
+        <button onClick={() => setOpen(true)}>Add new User</button>
       </div>
-      <DataTable columns={columns} rows={userRows} />
+      <DataTable columns={columns} rows={userRows} slug={""} />
+      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
     </div>
   );
 };
