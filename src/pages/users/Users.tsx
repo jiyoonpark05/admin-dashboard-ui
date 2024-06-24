80f52124ec8af55ac1486a61350b3584bccfe0db
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-24 18:36:08 +0200

    [add] data table

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,10 +1,73 @@
+import { GridColDef } from "@mui/x-data-grid";
+import DataTable from "../../components/dataTable/DataTable";
 import "./users.scss";
+import { userRows } from "../../data";
 const Users = () => {
+  const columns: GridColDef<(typeof rows)[number]>[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "avatar",
+      headerName: "Avatar",
+      width: 100,
+      renderCell: (params) => {
+        return <img src={params.row.img || "/noavatar.png"} alt="" />;
+      },
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 100,
+      renderCell: (params) => {
+        return (
+          <div className="action">
+            <div className="view">View</div>
+            <div className="delete">Delete</div>
+          </div>
+        );
+      },
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      type: "boolean",
+    },
+    {
+      field: "firstName",
+      headerName: "First name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "lastName",
+      headerName: "Last name",
+      width: 150,
+      editable: true,
+    },
+    {
+      field: "age",
+      headerName: "Age",
+      type: "number",
+      width: 110,
+      editable: true,
+    },
+    {
+      field: "fullName",
+      headerName: "Full name",
+      description: "This column has a value getter and is not sortable.",
+      sortable: false,
+      width: 160,
+      valueGetter: (value, row) =>
+        `${row.firstName || ""} ${row.lastName || ""}`,
+    },
+  ];
   return (
-    <div className="home">
+    <div className="users">
       <div className="info">
         <h1>Users</h1>
+        <button>Add new User</button>
       </div>
+      <DataTable columns={columns} rows={userRows} />
     </div>
   );
 };
