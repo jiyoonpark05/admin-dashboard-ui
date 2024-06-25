1aa24ea57669eecf9d99ff6095e08f49a7299ac9
jiyoonpark05 <jiyoonpark05@gmail.com>
2024-06-25 12:54:29 +0200

    [init] react-query

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import Login from "./pages/login/Login";
 import "./styles/global.scss";
 import User from "./components/user/User";
 import Product from "./components/product/Product";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   const Layout = () => {
@@ -20,7 +23,9 @@ function App() {
             <Menu />
           </div>
           <div className="contentContainer">
-            <Outlet />
+            <QueryClientProvider client={queryClient}>
+              <Outlet />
+            </QueryClientProvider>
           </div>
         </div>
         <Footer />
